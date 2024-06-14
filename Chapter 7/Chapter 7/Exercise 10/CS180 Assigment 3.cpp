9e5987d2795eb02828aee708186187e3231ae308
PaigeSu1 <164946572+PaigeSu1@users.noreply.github.com>
2024-06-14 17:18:51 -0500

    Update CS180 Assigment 3.cpp

--- Chapter 7/Chapter 7/Exercise 10/CS180 Assigment 3.cpp
+++ Chapter 7/Chapter 7/Exercise 10/CS180 Assigment 3.cpp
@@ -1,15 +1,3 @@
-//assigment 3
-
-#include<iostream>
-#include<cmath>
-#include<string>
-using namespace std;
-
-
-//Defined an enum type of WeekDays to represent the week of days Sunday-Saturday
-
-enum WeekDays
-
 
 
 
